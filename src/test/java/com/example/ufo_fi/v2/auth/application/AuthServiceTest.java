47384de9d8cffdca8cbd828e069b63d45d9c98eb
chungjeongsu <wjdwlgh2000@gmail.com>
2025-09-22 07:30:58 +0900

    test: AuthServiceTest application단 테스트 추가
    - 유저의 정보가 저장이 잘 되는가 확인

---
+++ src/test/java/com/example/ufo_fi/v2/auth/application/AuthServiceTest.java
@@ -0,0 +1,38 @@
+package com.example.ufo_fi.v2.auth.application;
+
+import com.example.ufo_fi.v2.auth.domain.OAuthUserInfo;
+import com.example.ufo_fi.v2.auth.domain.kakao.KakaoAccount;
+import com.example.ufo_fi.v2.auth.domain.kakao.KakaoUserInfo;
+import com.example.ufo_fi.v2.notification.send.application.WebPushClient;
+import com.example.ufo_fi.v2.user.domain.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class AuthServiceTest {
+
+    @Autowired
+    private AuthService authService;
+
+    @DisplayName("로그인 시 User가 저장됨을 확인할 수 있다.")
+    @Test
+    public void loginTest() {
+        //given
+        KakaoAccount kakaoAccount = new KakaoAccount("test2000@naver.com");
+        OAuthUserInfo oAuthUserInfo = new KakaoUserInfo("test-providerId", kakaoAccount);
+
+        //when
+        User user = authService.login(oAuthUserInfo);
+
+        //then
+        Assertions.assertThat(user)
+                .extracting("kakaoId", "email")
+                .contains("kakaotest-providerId", "test2000@naver.com");
+    }
+}
