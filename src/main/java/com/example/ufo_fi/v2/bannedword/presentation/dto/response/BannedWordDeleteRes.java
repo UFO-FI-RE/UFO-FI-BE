286ffeebd2cf114d3857e68aba3f2644a8203604
moomin <stl9955@naver.com>
2025-09-24 22:32:25 +0900

    refactor: BannedWord 도메인 수정

--- src/main/java/com/example/ufo_fi/v2/bannedword/presentation/dto/response/BannedWordDeleteRes.java
+++ src/main/java/com/example/ufo_fi/v2/bannedword/presentation/dto/response/BannedWordDeleteRes.java
@@ -1,6 +1,7 @@
 package com.example.ufo_fi.v2.bannedword.presentation.dto.response;
 
 
+import com.example.ufo_fi.v2.bannedword.domain.BannedWord;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,4 +16,10 @@ public class BannedWordDeleteRes {
 
     @Schema(description = "금칙어 ID", example = "1")
     private Long id;
+
+    public static BannedWordDeleteRes from(BannedWord bannedWord){
+        return BannedWordDeleteRes.builder()
+                .id(bannedWord.getId())
+                .build();
+    }
 }
