d2497395f9670085988f157866b6428d6b89f3d4
chungjeongsu <wjdwlgh2000@gmail.com>
2025-09-25 07:18:37 +0900

    refactor: TradePost 도메인 manager, mapper 삭제 리팩터링

--- src/main/java/com/example/ufo_fi/v2/order/application/bulk/BulkPurchaseContext.java
+++ src/main/java/com/example/ufo_fi/v2/order/application/bulk/BulkPurchaseContext.java
@@ -1,9 +1,13 @@
 package com.example.ufo_fi.v2.order.application.bulk;
 
+import com.example.ufo_fi.global.exception.GlobalException;
 import com.example.ufo_fi.v2.tradepost.domain.TradePost;
 import com.example.ufo_fi.v2.tradepost.domain.TradePostManager;
 import com.example.ufo_fi.v2.tradepost.domain.TradePostStatus;
 import java.util.List;
+
+import com.example.ufo_fi.v2.tradepost.exception.TradePostErrorCode;
+import com.example.ufo_fi.v2.tradepost.persistence.TradePostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +15,9 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class BulkPurchaseContext {
 
-    private final TradePostManager tradePostManager;
+    //private final TradePostManager tradePostManager;
+
+    private final TradePostRepository tradePostRepository;
     private final BulkPurchaseSuccessHandler bulkPurchaseSuccessHandler;
     private final BulkPurchaseFailureHandler bulkPurchaseFailureHandler;
 
@@ -21,7 +27,8 @@ public class BulkPurchaseContext {
         List<Long> sortedPostIds = postIds.stream().sorted().toList();
 
         sortedPostIds.forEach(sortedPostId -> {
-            TradePost tradePost = tradePostManager.findByIdWithLock(sortedPostId);
+            TradePost tradePost = tradePostRepository.findByIdWithLock(sortedPostId)
+                    .orElseThrow(() -> new GlobalException(TradePostErrorCode.TRADE_POST_NOT_FOUND));
             if (!tradePost.getTradePostStatus().equals(TradePostStatus.SELLING)) {
                 bulkPurchaseFailureHandler.handleFailure(tradePost, buyerId, purchaseResult);
             } else {
