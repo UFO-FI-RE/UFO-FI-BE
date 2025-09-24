f0dd021feb44a86c52bdd43efe991a902b7b4e74
chungjeongsu <wjdwlgh2000@gmail.com>
2025-09-24 18:26:49 +0900

    refactor: Report 도메인 manager, mapper 삭제 리팩터링

--- src/main/java/com/example/ufo_fi/v2/report/domain/Report.java
+++ src/main/java/com/example/ufo_fi/v2/report/domain/Report.java
@@ -1,5 +1,7 @@
 package com.example.ufo_fi.v2.report.domain;
 
+import com.example.ufo_fi.global.exception.GlobalException;
+import com.example.ufo_fi.v2.report.exception.ReportErrorCode;
 import com.example.ufo_fi.v2.report.presentation.dto.request.ReportCreateReq;
 import com.example.ufo_fi.v2.tradepost.domain.TradePost;
 import com.example.ufo_fi.v2.user.domain.User;
@@ -15,23 +17,16 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import java.util.Objects;
+
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
-@Table(
-    name = "reports",
-    uniqueConstraints = @UniqueConstraint(columnNames = {"trade_post_id", "reporting_user_id"})
-)
+@Table(name = "reports")
 @Getter
-@Builder
 @NoArgsConstructor
-@AllArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 public class Report {
 
@@ -60,16 +55,48 @@ public class Report {
     @JoinColumn(name = "trade_post_id", nullable = false)
     private TradePost tradePost;
 
-    public static Report of(
-        User reportingUser,
-        User reportedUser,
-        TradePost tradePost,
-        ReportCreateReq reportCreateReq) {
+    @Builder(access = AccessLevel.PRIVATE)
+    public Report(String content, User reportedUser, User reportingUser, TradePost tradePost) {
+        this.content = requireContent(content);
+        this.reportedUser = requireReportedUser(reportedUser);
+        this.reportingUser = requireReportingUser(reportingUser);
+        this.tradePost = requireTradePost(tradePost);
+        validateReportUser(reportedUser, reportingUser);
+    }
+
+    public static Report of(User reportingUser, User reportedUser, TradePost tradePost, String content) {
         return Report.builder()
-            .content(reportCreateReq.getContent())
+            .content(content)
             .reportedUser(reportedUser)
             .reportingUser(reportingUser)
             .tradePost(tradePost)
             .build();
+    }
+
+    //불변식 모음
+    private String requireContent(String content) {
+        if(content == null || content.isEmpty()) throw new GlobalException(ReportErrorCode.CONTENT_NOT_NULL);
+        if(content.length() > 100) throw new GlobalException(ReportErrorCode.CONTENT_TOO_LONG);
+        return content;
+    }
+
+    private User requireReportedUser(User user) {
+        if(user == null) throw new GlobalException(ReportErrorCode.REPORTED_USER_NOT_NULL);
+        return user;
+    }
+
+    private User requireReportingUser(User user) {
+        if(user == null) throw new GlobalException(ReportErrorCode.REPORTING_USER_NOT_NULL);
+        return user;
+    }
+
+    private TradePost requireTradePost(TradePost tradePost) {
+        if(tradePost == null) throw new GlobalException(ReportErrorCode.TRADE_POST_NOT_NULL);
+        return tradePost;
+    }
+
+    private void validateReportUser(User reportedUser, User reportingUser) {
+        if(Objects.equals(reportedUser.getId(), reportingUser.getId()))
+            throw new GlobalException(ReportErrorCode.REPORTED_REPORTING_USER_NOT_EQUAL);
     }
 }
--- src/main/java/com/example/ufo_fi/v2/report/exception/ReportErrorCode.java
+++ src/main/java/com/example/ufo_fi/v2/report/exception/ReportErrorCode.java
@@ -8,8 +8,14 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ReportErrorCode implements ErrorCode {
-    NO_REPORTED_USER(HttpStatus.NOT_FOUND, "신고 대상 유저를 찾을 수 없습니다.")
-    ;
+    NOT_FOUND_REPORTING_USER(HttpStatus.NOT_FOUND, "신고자 정보가 없습니다."),
+    NOT_FOUND_REPORTED_USER(HttpStatus.NOT_FOUND, "신고 대상 유저를 찾을 수 없습니다."),
+    CONTENT_NOT_NULL(HttpStatus.BAD_REQUEST, "Content는 null이 될 수 없습니다."),
+    CONTENT_TOO_LONG(HttpStatus.BAD_REQUEST, "Content가 너무 깁니다."),
+    REPORTED_USER_NOT_NULL(HttpStatus.BAD_REQUEST, "피신고자는 null이 될 수 없습니다."),
+    REPORTING_USER_NOT_NULL(HttpStatus.BAD_REQUEST, "신고자는 null이 될 수 없습니다."),
+    TRADE_POST_NOT_NULL(HttpStatus.BAD_REQUEST, "신고 게시물은 null이 될 수 없습니다."),
+    REPORTED_REPORTING_USER_NOT_EQUAL(HttpStatus.BAD_REQUEST, "신고자와 피신고자는 같을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
