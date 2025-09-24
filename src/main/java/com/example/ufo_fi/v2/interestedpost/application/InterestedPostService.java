12ae016ac429a37260ae244ea01c2f2a5aad965a
moomin <stl9955@naver.com>
2025-09-25 01:21:23 +0900

    refactor: interestedPost 도메인 수정

--- src/main/java/com/example/ufo_fi/v2/interestedpost/application/InterestedPostService.java
+++ src/main/java/com/example/ufo_fi/v2/interestedpost/application/InterestedPostService.java
@@ -1,8 +1,11 @@
 package com.example.ufo_fi.v2.interestedpost.application;
 
+import com.example.ufo_fi.global.exception.GlobalException;
 import com.example.ufo_fi.v2.interestedpost.domain.InterestedCarriers;
 import com.example.ufo_fi.v2.interestedpost.domain.InterestedPost;
 import com.example.ufo_fi.v2.interestedpost.domain.InterestedPostManager;
+import com.example.ufo_fi.v2.interestedpost.exception.InterestedPostErrorCode;
+import com.example.ufo_fi.v2.interestedpost.persistence.InterestedPostRepository;
 import com.example.ufo_fi.v2.interestedpost.presentation.dto.request.InterestedPostUpdateReq;
 import com.example.ufo_fi.v2.interestedpost.presentation.dto.response.InterestedPostRes;
 import com.example.ufo_fi.v2.user.domain.User;
@@ -18,26 +21,39 @@ public class InterestedPostService {
 
     private final InterestedPostManager interestedPostManager;
     private final InterestedPostMapper interestedPostMapper;
+
+    private final InterestedPostRepository interestedPostRepository;
     private final EntityManager entityManager;
 
     public void updateInterestedPost(Long userId, InterestedPostUpdateReq request) {
 
+        // before
+        // User userProxy = entityManager.getReference(User.class, userId);
+        // InterestedPost interestedPost = interestedPostManager.findByUser(userProxy);
+        // int carrierBit = interestedPostManager.encodeCarriers(request.getCarriers());
+        // interestedPostManager.updateInterestedPost(interestedPost, request, carrierBit);
+
         User userProxy = entityManager.getReference(User.class, userId);
-        InterestedPost interestedPost = interestedPostManager.findByUser(userProxy);
+        InterestedPost interestedPost = interestedPostRepository.findByUser(userProxy)
+                .orElseThrow(() -> new GlobalException(InterestedPostErrorCode.NO_INTERESTED_POST));
 
-        int carrierBit = interestedPostManager.encodeCarriers(request.getCarriers());
+        int carrierBit = InterestedCarriers.encode(request.getCarriers());
 
-        // TODO dto는 어디까지 전달
-        interestedPostManager.updateInterestedPost(interestedPost, request, carrierBit);
+        interestedPost.update(request, carrierBit);
     }
 
     public InterestedPostRes readInterestedPost(Long userId) {
 
+        // before
+        // User userProxy = entityManager.getReference(User.class, userId);
+        // InterestedPost interestedPost = interestedPostManager.findByUser(userProxy);
+
         User userProxy = entityManager.getReference(User.class, userId);
-        InterestedPost interestedPost = interestedPostManager.findByUser(userProxy);
+        InterestedPost interestedPost = interestedPostRepository.findByUser(userProxy)
+                .orElseThrow(() -> new GlobalException(InterestedPostErrorCode.NO_INTERESTED_POST));
 
-        List<InterestedCarriers> interestedCarriers = interestedPostManager.decodeCarrierBit(interestedPost.getCarrier());
+        List<InterestedCarriers> interestedCarriers = InterestedCarriers.decode(interestedPost.getCarrier());
 
-        return interestedPostMapper.toInterestedPostRes(interestedPost, interestedCarriers);
+        return InterestedPostRes.of(interestedCarriers, interestedPost);
     }
 }
--- src/main/java/com/example/ufo_fi/v2/interestedpost/domain/InterestedPost.java
+++ src/main/java/com/example/ufo_fi/v2/interestedpost/domain/InterestedPost.java
@@ -1,13 +1,12 @@
 package com.example.ufo_fi.v2.interestedpost.domain;
 
+import com.example.ufo_fi.global.exception.GlobalException;
+import com.example.ufo_fi.v2.interestedpost.exception.InterestedPostErrorCode;
 import com.example.ufo_fi.v2.interestedpost.presentation.dto.request.InterestedPostUpdateReq;
 import com.example.ufo_fi.v2.notification.setting.domain.Reputation;
 import com.example.ufo_fi.v2.user.domain.User;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Table(name = "interested_posts")
@@ -44,22 +43,70 @@ public class InterestedPost {
     @JoinColumn(name = "user_id")
     private User user;
 
+    @Builder(access = AccessLevel.PRIVATE)
+    private InterestedPost(int carrier, Integer interestedMaxCapacity, Integer interestedMinCapacity, Integer interestedMaxPrice, Integer interestedMinPrice, Reputation reputation, User user) {
+        this.carrier = carrier;
+        this.interestedMaxCapacity = validateInterestedMaxCapacity(interestedMaxCapacity);
+        this.interestedMinCapacity = validateInterestedMinCapacity(interestedMinCapacity);
+        this.interestedMaxPrice = validateInterestedMaxPrice(interestedMaxPrice);
+        this.interestedMinPrice = validateInterestedMinPrice(interestedMinPrice);
+        this.reputation = reputation;
+        this.user = requireUsers(user);
+    }
+
     public static InterestedPost from(User user) {
         return InterestedPost.builder()
                 .user(user)
                 .carrier(0)
-                .interestedMaxCapacity(0)
-                .interestedMinCapacity(0)
-                .interestedMaxPrice(0)
-                .interestedMinPrice(0)
+                .interestedMaxCapacity(5)
+                .interestedMinCapacity(5)
+                .interestedMaxPrice(50)
+                .interestedMinPrice(50)
                 .build();
     }
 
     public void update(InterestedPostUpdateReq request, int carrierBitmask) {
         this.carrier = carrierBitmask;
-        this.interestedMaxCapacity = request.getInterestedMaxCapacity();
-        this.interestedMinCapacity = request.getInterestedMinCapacity();
-        this.interestedMaxPrice = request.getInterestedMaxPrice();
-        this.interestedMinPrice = request.getInterestedMinPrice();
+        this.interestedMaxCapacity = validateInterestedMaxCapacity(request.getInterestedMaxCapacity());
+        this.interestedMinCapacity = validateInterestedMinCapacity(request.getInterestedMinCapacity());
+        this.interestedMaxPrice = validateInterestedMaxPrice(request.getInterestedMaxPrice());
+        this.interestedMinPrice = validateInterestedMinPrice(request.getInterestedMinPrice());
+    }
+
+    // 불변식쓰
+    // TODO : 추후 불변식 네이밍 규칙 정하기
+    private User requireUsers(User user){
+        if(user == null){
+            throw new GlobalException(InterestedPostErrorCode.INTERESTED_POST_USER_NOT_NULL);
+        }
+        return user;
+    }
+
+    private int validateInterestedMaxCapacity(int interestedMaxCapacity) {
+        if(interestedMaxCapacity > 10){
+            throw new GlobalException(InterestedPostErrorCode.INTERESTED_POST_MAX_CAPACITY_EXCEEDED);
+        }
+        return interestedMaxCapacity;
+    }
+
+    private int validateInterestedMinCapacity(int interestedMinCapacity) {
+        if(interestedMinCapacity <= 0) {
+            throw new GlobalException(InterestedPostErrorCode.INTERESTED_POST_MIN_CAPACITY_UNDERFLOW);
+        }
+        return interestedMinCapacity;
+    }
+
+    private int validateInterestedMaxPrice(int interestedMaxPrice) {
+        if(interestedMaxPrice > 100) {
+            throw new GlobalException(InterestedPostErrorCode.INTERESTED_POST_MAX_PRICE_EXCEEDED);
+        }
+        return interestedMaxPrice;
+    }
+
+    private int validateInterestedMinPrice(int interestedMinPrice) {
+        if(interestedMinPrice <= 0){
+            throw new GlobalException(InterestedPostErrorCode.INTERESTED_POST_MIN_PRICE_UNDERFLOW);
+        }
+        return interestedMinPrice;
     }
 }
